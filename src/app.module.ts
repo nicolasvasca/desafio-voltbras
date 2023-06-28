1579a4b0f9a09da0acb582e70b94f97a6c745c34
Nicolas Vasca <64164947+nicolasvasca@users.noreply.github.com>
2023-06-27 22:38:35 -0300

    Feat/user (#3)
    * feat:  add pg and  typeorm
    
    * feat: add apollo graphql
    
    * feat: add user
    
    * feat: export user services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UserModule } from './infra/ioc/user.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/application/services/user.service.ts
@@ -0,0 +1,66 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../../domain/models/user.entity';
+import { CreateUserInput } from '../../presentation/dtos/user/create-user.input';
+import { UpdateUserInput } from '../../presentation/dtos/user/update-user.input';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findAllUsers(): Promise<User[]> {
+    const users = await this.userRepository.find();
+    return users;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.userRepository.findOneById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async createUser(data: CreateUserInput): Promise<User> {
+    const user = this.userRepository.create(data);
+    const userSaved = await this.userRepository.save(user);
+
+    if (!userSaved) {
+      throw new InternalServerErrorException(
+        'Problem to create a user. Try again',
+      );
+    }
+
+    return userSaved;
+  }
+
+  async updateUser(id: string, data: UpdateUserInput): Promise<User> {
+    const user = await this.findById(id);
+
+    await this.userRepository.update(user, { ...data });
+
+    const userUpdated = this.userRepository.create({ ...user, ...data });
+
+    return userUpdated;
+  }
+
+  async deleteUser(id: string): Promise<boolean> {
+    const user = await this.findById(id);
+
+    const deleted = await this.userRepository.delete(user);
+
+    if (deleted) {
+      return true;
+    }
+
+    return false;
+  }
+}
